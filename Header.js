a5d2ca88b8daaaf7375210c2abd72c4b4902cd14
Bustervro <126031898+Bustervro@users.noreply.github.com>
2026-03-22 19:30:40 -0700

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Modular React Application</h1>
+      <p>Using default and named imports/exports</p>
+    </header>
+  );
+}
